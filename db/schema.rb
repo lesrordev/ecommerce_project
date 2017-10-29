809967c2e440490e8b8e2eec22a39d54319ca896
lesrordev <lesliecooper691@gmail.com>
2017-10-29 02:17:40 +0000

    Create orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029021505) do
+ActiveRecord::Schema.define(version: 20171029021702) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "description"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 20171029021505) do
     t.string "email"
     t.string "phone"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.decimal "pst"
+    t.decimal "gst"
+    t.decimal "hst"
+    t.integer "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
