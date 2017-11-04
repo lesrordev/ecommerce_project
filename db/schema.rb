cc473601c76af91bbf048a7540f449a20aae808f
lesrordev <lesliecooper691@gmail.com>
2017-11-04 14:31:40 +0000

    Added Province foreign key in Address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102131847) do
+ActiveRecord::Schema.define(version: 20171104142751) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20171102131847) do
     t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "province_id"
+    t.index ["province_id"], name: "index_addresses_on_province_id"
   end
 
   create_table "admin_users", force: :cascade do |t|
