e8e56a85344c3fe79a63be027d1d4ae729e8fd14
lesrordev <lesliecooper691@gmail.com>
2017-11-04 14:34:28 +0000

    Added Customer foreign key in Order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104142751) do
+ActiveRecord::Schema.define(version: 20171104143244) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 20171104142751) do
     t.decimal "hst"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id"
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "product_comments", force: :cascade do |t|
