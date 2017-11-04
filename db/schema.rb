4a168a43cab651ff0aba28346f05b694e18d4ee7
lesrordev <lesliecooper691@gmail.com>
2017-11-04 15:12:51 +0000

    Added image column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104150445) do
+ActiveRecord::Schema.define(version: 20171104151046) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20171104150445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
