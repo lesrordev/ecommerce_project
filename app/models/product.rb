f2c657020dcf99a4a37345f0f269e90fbe897be5
lesrordev <lesliecooper691@gmail.com>
2017-11-04 15:09:57 +0000

    Created relationships in the categoy and product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :product_comments
   has_many :order_items
   has_many :orders, :through => :order_items
+  belongs_to :category
 
   validates :name, :price, :stock_quantity, presence: true
   validates :price, :stock_quantity, numericality: { greater_than: 0 }
