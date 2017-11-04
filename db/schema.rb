3c02bba51f326a1f7dfd3b04d7f1e0f3702a0c4b
lesrordev <lesliecooper691@gmail.com>
2017-11-04 16:05:20 +0000

    Removed category column from Products (a category table exists)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104151046) do
+ActiveRecord::Schema.define(version: 20171104160216) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -120,7 +120,6 @@ ActiveRecord::Schema.define(version: 20171104151046) do
     t.string "description"
     t.decimal "price"
     t.integer "stock_quantity"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
