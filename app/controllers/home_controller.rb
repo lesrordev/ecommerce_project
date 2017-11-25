e221cda3501d36a3fd0b5f32f063472d0740e5be
lesrordev <lesliecooper691@gmail.com>
2017-11-25 07:41:39 -0600

    Added shopping cart in session and flash notice

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :initialize_session
+
   def index
     search_param = params[:search]
 
@@ -7,5 +9,16 @@ class HomeController < ApplicationController
     else
       @products = Product.order(:name).page params[:page]
     end
+  end
+
+  def add_product_to_cart
+    id = params[:id].to_i
+    session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
+    flash[:notice] = "Successfully added to shopping cart."
+    redirect_back(fallback_location: root_path)
+  end
+
+  def initialize_session
+    session[:shopping_cart] ||= []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+  resources :home do
+    member do
+      post :add_product_to_cart
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
